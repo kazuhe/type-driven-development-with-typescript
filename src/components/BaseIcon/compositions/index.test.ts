8971b048e82e4e8768af813f07b8a4a5e434ef80
kazuhe <omc.ohara@gmail.com>
2021-08-02 22:54:49 +0900

    toggleDisplayOfIconSelectionModalのテストを記述

--- src/components/BaseIcon/compositions/index.test.ts
+++ src/components/BaseIcon/compositions/index.test.ts
@@ -1,12 +1,25 @@
+import { ref } from 'vue'
 import { toggleDisplayOfIconSelectionModal, changeIcon } from '.'
 
 describe('toggleDisplayOfIconSelectionModal', () => {
-  test.todo(
-    'アイコン選択画面が「表示されている」場合は関数の実行によって非表示になること'
-  )
-  test.todo(
-    'アイコン選択画面が「表示されていない」場合は関数の実行によって表示されること'
-  )
+  test('アイコン選択画面が「表示されている」場合は関数の実行によって非表示になること', () => {
+    const isShownIconSelectionModal = ref(true)
+    const testTarget = toggleDisplayOfIconSelectionModal(
+      isShownIconSelectionModal
+    )
+    testTarget()
+
+    expect(isShownIconSelectionModal.value).toBe(false)
+  })
+  test('アイコン選択画面が「表示されていない」場合は関数の実行によって表示されること', () => {
+    const isShownIconSelectionModal = ref(false)
+    const testTarget = toggleDisplayOfIconSelectionModal(
+      isShownIconSelectionModal
+    )
+    testTarget()
+
+    expect(isShownIconSelectionModal.value).toBe(true)
+  })
 })
 
 describe('changeIcon', () => {
