a77e57948b15139916dec4910b4dffe0adc6c5da
kazuhe <omc.ohara@gmail.com>
2021-08-02 22:50:02 +0900

    jest.config.jsを調整

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,10 @@
 module.exports = {
   preset: '@vue/cli-plugin-unit-jest/presets/typescript',
+  moduleFileExtensions: ['ts', 'js', 'json', 'vue'],
   transform: {
     '^.+\\.vue$': 'vue-jest',
   },
+  testMatch: [
+    '<rootDir>/(tests/unit/**/*.spec.(js|jsx|ts|tsx)|**/*.test.(js|jsx|ts|tsx))',
+  ],
 }
