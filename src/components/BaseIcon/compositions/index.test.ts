2c082bc1f693b179f91700c5799a8c2978da40ed
kazuhe <omc.ohara@gmail.com>
2021-08-02 22:42:10 +0900

    テストのtodoを記述

---
+++ src/components/BaseIcon/compositions/index.test.ts
@@ -0,0 +1,15 @@
+import { toggleDisplayOfIconSelectionModal, changeIcon } from '.'
+
+describe('toggleDisplayOfIconSelectionModal', () => {
+  test.todo(
+    'アイコン選択画面が「表示されている」場合は関数の実行によって非表示になること'
+  )
+  test.todo(
+    'アイコン選択画面が「表示されていない」場合は関数の実行によって表示されること'
+  )
+})
+
+describe('changeIcon', () => {
+  test.todo('iconが既存のアイコンから新しいアイコンに変更されること')
+  test.todo('toggleDisplayOfIconSelectionModalがコールされていること')
+})
